38a6750e3b95dc03b373756fec4671f6d944f619
Muhammad Ghozi <ghozi.mghozi@gmail.com>
2017-11-20 08:43:44 +0700

    update change paramter for change adapter

--- app/src/main/java/id/strade/android/buyer/CustomAdapter.kt
+++ app/src/main/java/id/strade/android/buyer/CustomAdapter.kt
@@ -3,17 +3,22 @@ package id.strade.android.buyer
 /**
  * Created by ARSnova on 18/11/2017.
  */
+import android.content.Context
+import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
+import id.strade.android.buyer.model.Item
 import id.strade.android.buyer.model.Store
 import kotlinx.android.synthetic.main.list_layout.view.*
 import org.w3c.dom.Text
 
 
-class CustomAdapter(val userList: ArrayList<Store>) : RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
+class CustomAdapter(val userList: ArrayList<Store>, val rv: RecyclerView,val ctx:Context) : RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
 
     //this method is returning the view for each item in the list
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomAdapter.ViewHolder {
@@ -40,6 +45,20 @@ class CustomAdapter(val userList: ArrayList<Store>) : RecyclerView.Adapter<Custo
             name.text = user.name
             openTime.text = user.openTime
             closeTime.text = user.closeTime
+
+            itemView.setOnClickListener(View.OnClickListener {
+                val list_adapter = SellerAdapter(getItemLists())
+                rv.layoutManager = LinearLayoutManager(ctx, LinearLayout.VERTICAL, false)
+                rv.adapter = list_adapter
+            })
+        }
+        fun getItemLists(): ArrayList<Item> {
+            var lists = ArrayList<Item>()
+            lists.add(Item("Item1",10000))
+            lists.add(Item("Item2",20000))
+            lists.add(Item("Item3",30000))
+            return lists;
         }
     }
+
 }
