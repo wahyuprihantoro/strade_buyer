e5dbff312eeb26e1a2fb6986c80855f605ca84d8
Muhammad Ghozi <ghozi.mghozi@gmail.com>
2017-12-20 01:54:04 +0700

    menyesuaikan model category

--- app/src/main/java/id/strade/android/buyer/model/Category.kt
+++ app/src/main/java/id/strade/android/buyer/model/Category.kt
@@ -3,14 +3,13 @@ package id.strade.android.buyer.model
 /**
  * Created by ARSnova on 25/11/2017.
  */
-class Category(title:String,url:String,detail:String){
+class Category(title:String,url:String,id:Int){
     var title: String? = null
     var url_image: String? = null
-    var detail: String? = null
-
+    var id: Int? =null
     init{
         this.title=title
         url_image = url
-        this.detail = detail
+        this.id = id
     }
 }
