f21d4f2f2fbff2e3d779e01b96a02967ad0d0af8
Muhammad Ghozi <ghozi.mghozi@gmail.com>
2017-12-20 01:52:32 +0700

    call retrofit for store by category

--- app/src/main/java/id/strade/android/buyer/SellerListActivity.kt
+++ app/src/main/java/id/strade/android/buyer/SellerListActivity.kt
@@ -1,33 +1,88 @@
 package id.strade.android.buyer
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.MenuItem
+import android.widget.Toast
+import com.google.gson.Gson
 import id.strade.android.buyer.adapter.SellerAdapter
 import id.strade.android.buyer.model.Store
+import id.strade.android.buyer.model.User
+import id.strade.android.buyer.network.ApiClient
+import id.strade.android.buyer.network.response.StoreByCategoryResponse
+import id.strade.android.buyer.network.response.UserResponse
 import org.androidannotations.annotations.AfterViews
+import org.androidannotations.annotations.Bean
 import org.androidannotations.annotations.EActivity
 import org.androidannotations.annotations.ViewById
+import id.strade.android.buyer.network.service.StoreByCategoryService
+import id.strade.android.buyer.storage.Prefs
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 @EActivity(R.layout.activity_seller_list)
 open class SellerListActivity : AppCompatActivity() {
 
     @ViewById
     lateinit var rv: RecyclerView
+    @Bean
+    lateinit var apiClient: ApiClient
 
     @AfterViews
     fun init() {
+        var id = intent.getIntExtra("id",0)
+        var stores:ArrayList<Store> = ArrayList()
+        apiClient.getService(StoreByCategoryService::class.java).store_by_category(id).enqueue(object : Callback<StoreByCategoryResponse> {
+            override fun onResponse(call: Call<StoreByCategoryResponse>, response: Response<StoreByCategoryResponse>) {
+                if (response.isSuccessful) {
+                    var resp = response.body()
+                    if (resp != null && resp.status!!) {
+                        Log.d("wahyu", Gson().toJson(resp))
+                        var users:ArrayList<User> = resp.users!!
+                        users.forEach {
+                            stores.add(it.store!!)
+                        }
+                        Toast.makeText(applicationContext, "Berhasil", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(applicationContext, resp?.message, Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    Toast.makeText(applicationContext, "ada kesalahan pada server", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onFailure(call: Call<StoreByCategoryResponse>, t: Throwable) {
+                Toast.makeText(applicationContext, t.message, Toast.LENGTH_SHORT).show()
+            }
+        })
         if (supportActionBar != null) {
-            supportActionBar!!.title = "Sayur dan Buah"
+            if (id == 1){
+                supportActionBar!!.title = "Jajanan"
+            }else if(id == 2){
+                supportActionBar!!.title = "Minuman"
+            }else if(id == 3){
+                supportActionBar!!.title = "Makanan"
+            }else if(id == 4){
+                supportActionBar!!.title = "Jasa"
+            }else if(id == 5){
+                supportActionBar!!.title = "Perabotan"
+            }else if(id == 6){
+                supportActionBar!!.title = "Mainan"
+            }else if(id == 7){
+                supportActionBar!!.title = "Sayur dan Buah"
+            }
             supportActionBar!!.setDisplayHomeAsUpEnabled(true)
             supportActionBar!!.setDisplayShowHomeEnabled(true)
         }
-        val sellers: ArrayList<Store> = ArrayList()
-        sellers.add(Store(imageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQylvZ7R5ykRP9MrA34pSOaBICvEZkCaBB5bGxYbTR7hskpLX4A", status = "Buka", name = "Sayur Bang Ghozi", distance = 1.32, address = "Jalan Haji Kodja No 58"))
-        sellers.add(Store(imageUrl = "http://www.teropongbisnis.com/wp-content/uploads/2014/05/1.Menggiurkannya-Pedagang-Sayur-Keliling-2.jpg", status = "Buka", name = "Sayur Enak dan Murah", distance = 2.17, address = "Jalan Pala kali No 5"))
+//        val sellers: ArrayList<Store> = ArrayList()
+//        sellers.add(Store(imageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQylvZ7R5ykRP9MrA34pSOaBICvEZkCaBB5bGxYbTR7hskpLX4A", status = "Buka", name = "Sayur Bang Ghozi", distance = 1.32, address = "Jalan Haji Kodja No 58"))
+//        sellers.add(Store(imageUrl = "http://www.teropongbisnis.com/wp-content/uploads/2014/05/1.Menggiurkannya-Pedagang-Sayur-Keliling-2.jpg", status = "Buka", name = "Sayur Enak dan Murah", distance = 2.17, address = "Jalan Pala kali No 5"))
         rv.layoutManager = LinearLayoutManager(this)
-        rv.adapter = SellerAdapter(this, sellers)
+        rv.adapter = SellerAdapter(this, stores)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
